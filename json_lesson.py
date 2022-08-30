dc7a43c4412b4293ea402736f87e9dcf6466474d
NomadAllFather <Nomad153@mail.ru>
2022-08-30 14:23:35 +0300

    add file

--- json_lesson.py
+++ json_lesson.py
@@ -1,5 +1,12 @@
 import json
+import csv
 with open('json_text.json', 'r',) as f:
     data = json.load(f)
 for txt in data:
     print(txt)
+with open('json_test_01.csv', 'w',) as f:
+    fieldnames = txt[].keys()
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writerow()
+    for name in names:
+        writer.writerow(name)
