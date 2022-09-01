4fc8d061b6a8f4f15ce42377179baba116d586f1
NomadAllFather <Nomad153@mail.ru>
2022-09-01 10:58:41 +0300

    add file

--- TMS/tms_lessons/Lesson_2(4).py
+++ TMS/tms_lessons/Lesson_2(4).py
@@ -1,3 +1,7 @@
+#Ex. 4
+#Есть кортеж элементов CITIES = ('Minsk', 'Gomel', 'Mogivel', 'Orsha', 'Vitebsk', 'Grodno')
+#На базе него создать список и добавить новый город.
+
 CITIES = ('Minsk', 'Gomel', 'Mogivel', 'Orsha', 'Vitebsk', 'Grodno')
 a = list(CITIES)
 a.append('Lida')
