b1f1c64e57fba83f76423e2ce28534c5a3274975
NomadAllFather <Nomad153@mail.ru>
2022-09-01 10:52:22 +0300

    add file

--- TMS/tms_lessons/Lesson_2(2).py
+++ TMS/tms_lessons/Lesson_2(2).py
@@ -1,7 +1,3 @@
 NUMBERS = [1,1,1,2,2,2,3,3,3,4,5,6,7,7,8,8,0,9,9]
-spisok = {
-    'a': NUMBERS
-
-}
-b = set(spisok['a'])
-assert b == {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+numbers_dictionary = dict.fromkeys(NUMBERS, 'unique numbers')
+print(numbers_dictionary)
