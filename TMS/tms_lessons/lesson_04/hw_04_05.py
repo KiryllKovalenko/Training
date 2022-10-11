8e3ee983ca41029e91d44302cbaa512aa579f053
NomadAllFather <108832893+NomadAllFather@users.noreply.github.com>
2022-10-11 10:14:40 +0300

    Revert "Master"

--- TMS/tms_lessons/lesson_04/hw_04_05.py
+++
@@ -1,7 +0,0 @@
-"""Ex. 5
-Написать ф-ю, которая выводит сегодн дату, текущее время и приветствие."""
-from datetime import datetime
-def current_time():
-    current_date = datetime.now()
-    print(f"Привет пользователь.Сейчас {current_date}")
-current_time()
