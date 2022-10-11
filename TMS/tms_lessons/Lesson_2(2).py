dd1073868334debd89f4117d33d875fca1bd3433
Kirill Kovalenko <108832893+KiryllKovalenko@users.noreply.github.com>
2022-10-11 19:19:40 +0300

    Delete Lesson_2(2).py

--- TMS/tms_lessons/Lesson_2(2).py
+++
@@ -1,7 +0,0 @@
-#Ex. 2
-#Есть список NUMBERS, содержащий повторяющиеся целые числа.
-#С помощью словаря (dict) отфильтровать его и получить новый список с уникальными числами.
-
-NUMBERS = [1,1,1,2,2,2,3,3,3,4,5,6,7,7,8,8,0,9,9]
-numbers_dictionary = dict.fromkeys(NUMBERS, 'unique numbers')
-print(numbers_dictionary)
